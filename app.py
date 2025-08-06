35b8866330f8115371619147961c9fb97de90b91
shinopy275 <ssato87038@gmail.com>
2025-08-06 17:35:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -370,9 +370,9 @@ def plot_reason_yoy(df_src, store, latest, prev):
             y="件数:Q",
             # ★ 並びを明示的に固定 ★
             xOffset=alt.XOffset("年度:N",
-                               scale=alt.Scale(domain=[@latest, @prev])),
+                               scale=alt.Scale(domain=["前年", "今年"])),
                        color=alt.Color("年度:N",
-                            scale=alt.Scale(domain=[@latest,  @prev],
+                            scale=alt.Scale(domain=["前年", "今年"],
                                             range=['#4e79a7', '#f28e2b'])),  # 色も任意
             tooltip=["年度", "カテゴリ", "件数"],
         ).properties(width=400, height=300,
